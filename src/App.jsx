cf80580c16d791f65e74f224f38446c4a7f3e004
LazY <darshitmistry2@gmail.com>
2023-06-29 22:10:47 +0530

    Some update here

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
 import React from "react";
-import { ChakraProvider } from "@chakra-ui/react";
 import { Navbar } from "./components";
 
 const App = () => {
   return (
-    <ChakraProvider>
       <Navbar />
-    </ChakraProvider>
   );
 };
 
